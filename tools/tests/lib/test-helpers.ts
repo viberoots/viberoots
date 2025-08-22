5cd53ddf0b99500a9bf208a5cdfc081a3959fde4
Kilty McGowan <kiltyj@gmail.com>
2025-08-22 11:45:27 -0700

    test: include node_modules in temp repo rsync for Buck2 tests

---
+++ tools/tests/lib/test-helpers.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx-wrapper
+import * as fsp from "node:fs/promises";
+import path from "node:path";
+import os from "node:os";
+
+export async function rsyncRepoTo(tmp: string) {
+  await $`bash -lc 'rsync -a --exclude "buck-out" --exclude ".git" --exclude "libs" --exclude ".tmp" ./ ${tmp}/'`;
+}
+
+export async function mktemp(prefix = "test-") {
+  return await fsp.mkdtemp(path.join(os.tmpdir(), prefix));
+}
+
+export async function exists(p: string) {
+  try { await fsp.access(p); return true; } catch { return false; }
+}
